adfae4e791f6f0dacf58b5941e7cb29e6cad1b25
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-08-02 07:59:15 -0400

    added logging config file and started UML diagram

---
+++ src/main/java/com/revature/p0/App.java
@@ -0,0 +1,11 @@
+package com.revature.p0;
+
+import com.revature.p0.POJO.Faculty;
+
+public class App {
+
+    public static void main(String[] args) {
+        Faculty teacher = new Faculty();
+        teacher.doQuery("yoyo");
+    }
+}
---
+++ src/main/java/com/revature/p0/POJO/Faculty.java
@@ -0,0 +1,18 @@
+package com.revature.p0.POJO;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Faculty {
+    Logger logger = LogManager.getLogger("Faculty.class");
+
+    int id;
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+    
+
+
+}
