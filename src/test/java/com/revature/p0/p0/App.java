77c61dd7741120275c68550821ade985a2c5cbea
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-08-04 14:03:16 -0400

    added Router, ScreenRouter, and filled out WelcomeScreen render() implementation

--- src/test/java/com/revature/p0/p0/App.java
+++
@@ -1,9 +0,0 @@
-package com.revature.p0.p0;
-
-
-public class App {
-
-    public static void main(String[] args) {
-
-    }
-}
--- src/test/java/com/revature/p0/p0/respositries/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.p0.p0.respositries;
-
-public abstract class Repository<T> {
-    abstract void create(T type);
-    abstract T search(T type);
-    abstract T update(T type);
-    abstract void delete(T type);
-
-}
