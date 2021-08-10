4d613414990b13478605ff42e40da8f811803766
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-08-10 11:21:50 -0400

    course save implementation completed

--- src/main/java/com/revature/p0/repositories/CourseRepository.java
+++ src/main/java/com/revature/p0/repositories/CourseRepository.java
@@ -6,13 +6,14 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.revature.p0.documents.AppUser;
 import com.revature.p0.documents.Course;
+import com.revature.p0.exceptions.DataSourceException;
 import com.revature.p0.util.MongoClientFactory;
 import com.revature.p0.util.UserSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bson.Document;
 
-public class CourseRepository implements CrudRepository{
+public class CourseRepository implements CrudRepository<Course>{
     static final Logger logger = LogManager.getLogger(UserRepository.class);
     private UserSession session;
     MongoClient mongoClient;
@@ -43,17 +44,37 @@ public class CourseRepository implements CrudRepository{
     }
 
     @Override
-    public Object findById(String id) {
+    public Course findById(String id) {
         return null;
     }
 
     @Override
-    public void save(Object newResource) {
+    public void save(Course newCourse) {
+        try {
+            MongoCollection<Document> userCollection = projectDb.getCollection("courses");
+            //Check to make sure course tag does not already exist in database.
+            Document queryDoc = userCollection.find(new BasicDBObject("_id", newCourse.getCourseId())).first();
+            if(queryDoc != null) {
+                throw new RuntimeException(newCourse.getCourseId()+" already exists in "+userCollection.toString());
+            }
+            //Create course document and save to database.
+            Document newCourseDoc = newCourse.toDocument();
+            userCollection.insertOne(newCourseDoc);
 
+            //Query the doc we just saved and obtain the course ID
+            Document getIDDoc = userCollection.find(new BasicDBObject("coursetag", newCourse.getCourseTag())).first();
+            newCourse.setCourseId(getIDDoc.get("_id").toString());
+            logger.info("course "+newCourse.getCourseTag()+" saved to "+userCollection.getNamespace()+" with id "+newCourse.getCourseId());
+
+        } catch (Exception e) {
+            e.printStackTrace(); // TODO log this to a file
+            throw new DataSourceException("An unexpected exception occurred.", e);
+        }
     }
 
+
     @Override
-    public void update(Object updatedResource) {
+    public void update(Course updatedCourse) {
 
     }
 
--- src/main/java/com/revature/p0/services/CourseService.java
+++ src/main/java/com/revature/p0/services/CourseService.java
@@ -36,4 +36,6 @@ public class CourseService {
             return true;
         return false;
     }
+
+    
 }
